1d6bd8571b5d457ad4675ceb7c68c96726a43cce
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-02-23 11:41:32 -0500

    Implement preferences store.
    Can now store configuration settings via Eclipse.

--- com.laboki.eclipse.e4.plugin.autosave/src/com/laboki/eclipse/e4/plugin/autosave/automaticsaver/preferences/PreferencesStore.java
+++ com.laboki.eclipse.e4.plugin.autosave/src/com/laboki/eclipse/e4/plugin/autosave/automaticsaver/preferences/PreferencesStore.java
@@ -1,35 +1,78 @@
 package com.laboki.eclipse.e4.plugin.autosave.automaticsaver.preferences;
 
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.osgi.service.prefs.BackingStoreException;
+
+import com.laboki.eclipse.e4.plugin.autosave.AddonMetadata;
+
 public final class PreferencesStore {
 
 	private static final boolean CAN_CHECK_WARNINGS_DEFAULT_VALUE = false;
 	private static final boolean CAN_CHECK_ERRORS_DEFAULT_VALUE = true;
 	private static final boolean CAN_SAVE_AUTOMATICALLY_DEFAULT_VALUE = true;
 	private static final int SAVE_INTERVAL_IN_SECONDS_DEFAULT_VALUE = 5;
+	private static final String SAVE_AUTOMATICALLY_KEY = "saveAutomatically";
+	private static final String CHECK_WARNINGS_KEY = "checkWarnings";
+	private static final String CHECK_ERRORS_KEY = "checkErrors";
+	private static final String SAVE_INTERVAL_KEY = "saveIntervalInSeconds";
+	private static final IEclipsePreferences PREFERENCES = InstanceScope.INSTANCE.getNode(AddonMetadata.PLUGIN_NAME);
 
 	private PreferencesStore() {}
 
-	public static void setCanSaveAutomatically(@SuppressWarnings("unused") final boolean saveAutomatically) {}
+	public static void setCanSaveAutomatically(final boolean saveAutomatically) {
+		PreferencesStore.PREFERENCES.putBoolean(PreferencesStore.SAVE_AUTOMATICALLY_KEY, saveAutomatically);
+		PreferencesStore.flush();
+	}
 
 	public static boolean getCanSaveAutomatically() {
-		return PreferencesStore.CAN_SAVE_AUTOMATICALLY_DEFAULT_VALUE;
+		PreferencesStore.sync();
+		return PreferencesStore.PREFERENCES.getBoolean(PreferencesStore.SAVE_AUTOMATICALLY_KEY, PreferencesStore.CAN_SAVE_AUTOMATICALLY_DEFAULT_VALUE);
 	}
 
-	public static void setCanCheckWarnings(@SuppressWarnings("unused") final boolean checkWarnings) {}
+	public static void setCanCheckWarnings(final boolean checkWarnings) {
+		PreferencesStore.PREFERENCES.putBoolean(PreferencesStore.CHECK_WARNINGS_KEY, checkWarnings);
+		PreferencesStore.flush();
+	}
 
 	public static boolean getCanCheckWarnings() {
-		return PreferencesStore.CAN_CHECK_WARNINGS_DEFAULT_VALUE;
+		PreferencesStore.sync();
+		return PreferencesStore.PREFERENCES.getBoolean(PreferencesStore.CHECK_WARNINGS_KEY, PreferencesStore.CAN_CHECK_WARNINGS_DEFAULT_VALUE);
 	}
 
-	public static void setCanCheckErrors(@SuppressWarnings("unused") final boolean checkErrors) {}
+	public static void setCanCheckErrors(final boolean checkErrors) {
+		PreferencesStore.PREFERENCES.putBoolean(PreferencesStore.CHECK_ERRORS_KEY, checkErrors);
+		PreferencesStore.flush();
+	}
 
 	public static boolean getCanCheckErrors() {
-		return PreferencesStore.CAN_CHECK_ERRORS_DEFAULT_VALUE;
+		PreferencesStore.sync();
+		return PreferencesStore.PREFERENCES.getBoolean(PreferencesStore.CHECK_ERRORS_KEY, PreferencesStore.CAN_CHECK_ERRORS_DEFAULT_VALUE);
 	}
 
-	public static void setSaveIntervalInSeconds(@SuppressWarnings("unused") final int time) {}
+	public static void setSaveIntervalInSeconds(final int saveIntervalInSeconds) {
+		PreferencesStore.PREFERENCES.putInt(PreferencesStore.SAVE_INTERVAL_KEY, saveIntervalInSeconds);
+		PreferencesStore.flush();
+	}
 
 	public static int getSaveIntervalInSeconds() {
-		return PreferencesStore.SAVE_INTERVAL_IN_SECONDS_DEFAULT_VALUE;
+		PreferencesStore.sync();
+		return PreferencesStore.PREFERENCES.getInt(PreferencesStore.SAVE_INTERVAL_KEY, PreferencesStore.SAVE_INTERVAL_IN_SECONDS_DEFAULT_VALUE);
+	}
+
+	private static void flush() {
+		try {
+			PreferencesStore.PREFERENCES.flush();
+		} catch (final BackingStoreException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void sync() {
+		try {
+			PreferencesStore.PREFERENCES.sync();
+		} catch (final BackingStoreException e) {
+			e.printStackTrace();
+		}
 	}
 }
