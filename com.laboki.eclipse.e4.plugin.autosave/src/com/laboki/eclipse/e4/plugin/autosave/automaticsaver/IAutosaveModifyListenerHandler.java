c2b8879bbdef7babfbbfbff549a02568730c77f0
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-02-28 20:31:11 -0500

    Another error *sigh*

--- com.laboki.eclipse.e4.plugin.autosave/src/com/laboki/eclipse/e4/plugin/autosave/automaticsaver/IAutosaveModifyListenerHandler.java
+++
@@ -1,6 +0,0 @@
-package com.laboki.eclipse.e4.plugin.autosave.automaticsaver;
-
-interface IAutosaveModifyListenerHandler {
-
-	void modify();
-}
--- com.laboki.eclipse.e4.plugin.autosave/src/com/laboki/eclipse/e4/plugin/autosave/automaticsaver/IKeyListenersHandler.java
+++
@@ -1,8 +0,0 @@
-package com.laboki.eclipse.e4.plugin.autosave.automaticsaver;
-
-interface IKeyListenersHandler {
-
-	void keyPress();
-
-	void keyRelease();
-}
