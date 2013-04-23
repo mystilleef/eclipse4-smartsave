dcce18b595006de70636e2b73af09f3f1dee8dfe
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-04-23 16:01:39 -0400

    Add file syncer service.

--- com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/saver/SaverServices.java
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/saver/SaverServices.java
@@ -1,18 +1,45 @@
 package com.laboki.eclipse.plugin.smartsave.saver;
 
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.laboki.eclipse.plugin.smartsave.Instance;
 
 public final class SaverServices implements Instance {
 
-	public SaverServices() {}
+	private final List<Instance> instances = Lists.newArrayList();
+	private final EventBus eventBus = new EventBus();
 
 	@Override
 	public Instance begin() {
+		this.startServices();
 		return this;
+	}
+
+	private void startServices() {
+		this.startService(new FileSyncer(this.eventBus));
+	}
+
+	private void startService(final Instance instance) {
+		instance.begin();
+		this.instances.add(instance);
 	}
 
 	@Override
 	public Instance end() {
+		this.stopServices();
+		this.instances.clear();
 		return this;
+	}
+
+	private void stopServices() {
+		for (final Instance instance : ImmutableList.copyOf(this.instances))
+			this.stopService(instance);
+	}
+
+	private void stopService(final Instance instance) {
+		instance.end();
+		this.instances.remove(instance);
 	}
 }
