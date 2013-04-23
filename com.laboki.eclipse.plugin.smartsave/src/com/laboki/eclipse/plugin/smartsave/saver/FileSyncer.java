ecd1fc4242f643f720f16e53b1b3c173aeaad9ce
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-04-23 16:01:03 -0400

    Implement file syncing services.
    This syncs local resources in eclipse before saving. This allows use to
    make sure that annotation information in parts are not stale.
    Unfortunately, it make also massively slow eclipse down. Needs more
    testing.

---
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/saver/FileSyncer.java
@@ -0,0 +1,51 @@
+package com.laboki.eclipse.plugin.smartsave.saver;
+
+import org.eclipse.ui.IEditorPart;
+
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.Subscribe;
+import com.laboki.eclipse.plugin.smartsave.Instance;
+import com.laboki.eclipse.plugin.smartsave.Task;
+import com.laboki.eclipse.plugin.smartsave.saver.events.FinishedSyncFilesEvent;
+import com.laboki.eclipse.plugin.smartsave.saver.events.SyncFilesEvent;
+
+final class FileSyncer implements Instance {
+
+	private final EventBus eventBus;
+	private final IEditorPart editor = EditorContext.getEditor();
+
+	public FileSyncer(final EventBus eventBus) {
+		this.eventBus = eventBus;
+	}
+
+	@Subscribe
+	@AllowConcurrentEvents
+	public void syncFiles(@SuppressWarnings("unused") final SyncFilesEvent event) {
+		EditorContext.asyncExec(new Task("") {
+
+			@Override
+			public void execute() {
+				EditorContext.tryToSyncFile(FileSyncer.this.editor);
+				this.postEvent();
+			}
+
+			private void postEvent() {
+				FileSyncer.this.eventBus.post(new FinishedSyncFilesEvent());
+			}
+		});
+	}
+
+	@Override
+	public Instance begin() {
+		this.eventBus.register(this);
+		System.out.println("Starting file syncer service");
+		return this;
+	}
+
+	@Override
+	public Instance end() {
+		this.eventBus.unregister(this);
+		System.out.println("Stoping file syncer services");
+		return this;
+	}
+}
