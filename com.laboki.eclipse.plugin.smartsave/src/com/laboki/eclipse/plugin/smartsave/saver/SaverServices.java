c8d19f611f37248a8d488b26574c94ae74c5829e
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-04-23 15:29:52 -0400

    Add module responsible for creating/destroying saver services.

---
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/saver/SaverServices.java
@@ -0,0 +1,18 @@
+package com.laboki.eclipse.plugin.smartsave.saver;
+
+import com.laboki.eclipse.plugin.smartsave.Instance;
+
+public final class SaverServices implements Instance {
+
+	public SaverServices() {}
+
+	@Override
+	public Instance begin() {
+		return this;
+	}
+
+	@Override
+	public Instance end() {
+		return this;
+	}
+}
