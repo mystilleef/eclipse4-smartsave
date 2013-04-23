20e75f58605b488977efcc1fa5f4e187d76d3dbd
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-04-23 14:37:55 -0400

    Add event bus for message passing.

---
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/saver/EventBus.java
@@ -0,0 +1,33 @@
+package com.laboki.eclipse.plugin.smartsave.saver;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import com.google.common.eventbus.AsyncEventBus;
+import com.laboki.eclipse.plugin.smartsave.Task;
+
+final class EventBus {
+
+	private static final Executor EXECUTOR = Executors.newCachedThreadPool();
+	private final AsyncEventBus bus = new AsyncEventBus(EventBus.EXECUTOR);
+
+	public EventBus() {}
+
+	public void register(final Object object) {
+		this.bus.register(object);
+	}
+
+	public void unregister(final Object object) {
+		this.bus.unregister(object);
+	}
+
+	public void post(final Object object) {
+		EditorContext.asyncExec(new Task("") {
+
+			@Override
+			public void execute() {
+				EventBus.this.bus.post(object);
+			}
+		});
+	}
+}
