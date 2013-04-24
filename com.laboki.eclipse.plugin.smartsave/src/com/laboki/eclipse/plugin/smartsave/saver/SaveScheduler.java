878f0e11394f0dae989ee1240effc767edcba441
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-04-23 21:25:55 -0400

    Add object to schedule saving.

---
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/saver/SaveScheduler.java
@@ -0,0 +1,62 @@
+package com.laboki.eclipse.plugin.smartsave.saver;
+
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.Subscribe;
+import com.laboki.eclipse.plugin.smartsave.DelayedTask;
+import com.laboki.eclipse.plugin.smartsave.Instance;
+import com.laboki.eclipse.plugin.smartsave.Task;
+import com.laboki.eclipse.plugin.smartsave.saver.events.DisableSaveListenersEvent;
+import com.laboki.eclipse.plugin.smartsave.saver.events.EnableSaveListenersEvent;
+import com.laboki.eclipse.plugin.smartsave.saver.events.ScheduleSaveEvent;
+import com.laboki.eclipse.plugin.smartsave.saver.events.ScheduleSaveOnIdleEvent;
+
+public final class SaveScheduler implements Instance {
+
+	private final EventBus eventBus;
+
+	public SaveScheduler(final EventBus eventBus) {
+		this.eventBus = eventBus;
+	}
+
+	@Subscribe
+	@AllowConcurrentEvents
+	public void scheduleSave(@SuppressWarnings("unused") final ScheduleSaveEvent event) {
+		EditorContext.asyncExec(new DelayedTask(EditorContext.SCHEDULED_SAVER_TASK, EditorContext.SHORT_DELAY_TIME) {
+
+			@Override
+			public void execute() {
+				SaveScheduler.this.eventBus.post(new ScheduleSaveOnIdleEvent());
+			}
+		});
+	}
+
+	@Subscribe
+	@AllowConcurrentEvents
+	public void scheduleSave(@SuppressWarnings("unused") final EnableSaveListenersEvent event) {
+		EditorContext.asyncExec(new Task(EditorContext.SCHEDULED_SAVER_TASK) {
+
+			@Override
+			public void execute() {
+				EditorContext.scheduleSave(SaveScheduler.this.eventBus);
+			}
+		});
+	}
+
+	@Subscribe
+	@AllowConcurrentEvents
+	public static void cancelSaveJobs(@SuppressWarnings("unused") final DisableSaveListenersEvent event) {
+		EditorContext.cancelAllJobs();
+	}
+
+	@Override
+	public Instance begin() {
+		this.eventBus.register(this);
+		return this;
+	}
+
+	@Override
+	public Instance end() {
+		this.eventBus.unregister(this);
+		return this;
+	}
+}
