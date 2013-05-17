55346339b60defe2059bbc4468454fb288d659c1
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-05-17 11:04:59 -0400

    Slightly delay handling preferences change events.
    Prevents possible race condition.

--- com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/listeners/PreferenceChangeListener.java
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/listeners/PreferenceChangeListener.java
@@ -8,11 +8,13 @@ import com.laboki.eclipse.plugin.smartsave.events.PreferenceStoreChangeEvent;
 import com.laboki.eclipse.plugin.smartsave.instance.AbstractEventBusInstance;
 import com.laboki.eclipse.plugin.smartsave.instance.Instance;
 import com.laboki.eclipse.plugin.smartsave.preferences.Store;
+import com.laboki.eclipse.plugin.smartsave.saver.EditorContext;
 import com.laboki.eclipse.plugin.smartsave.saver.EventBus;
 import com.laboki.eclipse.plugin.smartsave.task.Task;
 
 public final class PreferenceChangeListener extends AbstractEventBusInstance implements IPreferenceChangeListener {
 
+	private static final String SMARTSAVE_PREFERENCE_CHANGE_EVENT_TASK = "smartsave preference change event listener";
 	private static final IEclipsePreferences PREFERENCES = Store.getPreferences();
 
 	public PreferenceChangeListener(final EventBus eventBus) {
@@ -21,7 +23,12 @@ public final class PreferenceChangeListener extends AbstractEventBusInstance imp
 
 	@Override
 	public void preferenceChange(final PreferenceChangeEvent event) {
-		new Task() {
+		new Task(PreferenceChangeListener.SMARTSAVE_PREFERENCE_CHANGE_EVENT_TASK, 250) {
+
+			@Override
+			public boolean shouldSchedule() {
+				return EditorContext.taskDoesNotExist(PreferenceChangeListener.SMARTSAVE_PREFERENCE_CHANGE_EVENT_TASK);
+			}
 
 			@Override
 			public void execute() {
