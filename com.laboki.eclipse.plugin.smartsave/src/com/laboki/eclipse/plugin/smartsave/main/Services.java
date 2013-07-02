cf1e31b6690362ca71860263f3b7823081a62686
Lateef Alabi-Oki <mystilleef@gmail.com>
2013-07-02 14:55:43 -0400

    Enable verify listener.

--- com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/main/Services.java
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/main/Services.java
@@ -10,6 +10,7 @@ import com.laboki.eclipse.plugin.smartsave.listeners.DirtyPartListener;
 import com.laboki.eclipse.plugin.smartsave.listeners.KeyEventListener;
 import com.laboki.eclipse.plugin.smartsave.listeners.ListenerSwitch;
 import com.laboki.eclipse.plugin.smartsave.listeners.PreferenceChangeListener;
+import com.laboki.eclipse.plugin.smartsave.listeners.VerifyEventListener;
 import com.laboki.eclipse.plugin.smartsave.preferences.Updater;
 
 public final class Services implements Instance {
@@ -26,7 +27,7 @@ public final class Services implements Instance {
 		this.startService(new Saver(EditorContext.EVENT_BUS));
 		this.startService(new FileSyncer(EditorContext.EVENT_BUS));
 		this.startService(new Scheduler(EditorContext.EVENT_BUS));
-		// FIXME: this.startService(new VerifyEventListener(EditorContext.EVENT_BUS));
+		this.startService(new VerifyEventListener(EditorContext.EVENT_BUS));
 		// FIXME: this.startService(new AnnotationsListener(EditorContext.EVENT_BUS));
 		this.startService(new KeyEventListener(EditorContext.EVENT_BUS));
 		this.startService(new ListenerSwitch(EditorContext.EVENT_BUS));
