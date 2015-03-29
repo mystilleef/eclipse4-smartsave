a2634ddd28c6dc271d962402683c54918202c1e0
Lateef Alabi-Oki <mystilleef@gmail.com>
2015-03-29 02:49:42 -0400

    Force save when editor loses focus.

--- com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/main/Services.java
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/main/Services.java
@@ -1,3 +1,4 @@
+
 package com.laboki.eclipse.plugin.smartsave.main;
 
 import java.util.List;
@@ -16,47 +17,48 @@ import com.laboki.eclipse.plugin.smartsave.preferences.Updater;
 
 public final class Services implements Instance {
 
-	private final List<Instance> instances = Lists.newArrayList();
+  private final List<Instance> instances = Lists.newArrayList();
 
-	@Override
-	public Instance begin() {
-		this.startServices();
-		return this;
-	}
+  @Override
+  public Instance begin() {
+    this.startServices();
+    return this;
+  }
 
-	private void startServices() {
-		this.startService(new Saver(EditorContext.EVENT_BUS));
-		this.startService(new FileSyncer(EditorContext.EVENT_BUS));
-		this.startService(new Scheduler(EditorContext.EVENT_BUS));
-		this.startService(new VerifyEventListener(EditorContext.EVENT_BUS));
-		this.startService(new AnnotationsListener(EditorContext.EVENT_BUS));
-		this.startService(new KeyEventListener(EditorContext.EVENT_BUS));
-		this.startService(new ListenerSwitch(EditorContext.EVENT_BUS));
-		this.startService(new CompletionListener(EditorContext.EVENT_BUS));
-		this.startService(new DirtyPartListener(EditorContext.EVENT_BUS));
-		this.startService(new Updater(EditorContext.EVENT_BUS));
-		this.startService(new PreferenceChangeListener(EditorContext.EVENT_BUS));
-	}
+  private void startServices() {
+    this.startService(new Saver(EditorContext.EVENT_BUS));
+    this.startService(new FileSyncer(EditorContext.EVENT_BUS));
+    this.startService(new Scheduler(EditorContext.EVENT_BUS));
+    this.startService(new VerifyEventListener(EditorContext.EVENT_BUS));
+    this.startService(new AnnotationsListener(EditorContext.EVENT_BUS));
+    this.startService(new KeyEventListener(EditorContext.EVENT_BUS));
+    this.startService(new ListenerSwitch(EditorContext.EVENT_BUS));
+    this.startService(new CompletionListener(EditorContext.EVENT_BUS));
+    this.startService(new DirtyPartListener(EditorContext.EVENT_BUS));
+    this.startService(new Updater(EditorContext.EVENT_BUS));
+    this.startService(new PreferenceChangeListener(EditorContext.EVENT_BUS));
+  }
 
-	private void startService(final Instance instance) {
-		instance.begin();
-		this.instances.add(instance);
-	}
+  private void startService(final Instance instance) {
+    instance.begin();
+    this.instances.add(instance);
+  }
 
-	@Override
-	public Instance end() {
-		this.stopServices();
-		this.instances.clear();
-		return this;
-	}
+  @Override
+  public Instance end() {
+    this.stopServices();
+    this.instances.clear();
+    return this;
+  }
 
-	private void stopServices() {
-		for (final Instance instance : ImmutableList.copyOf(this.instances))
-			this.stopService(instance);
-	}
+  private void stopServices() {
+    EditorContext.forceSave();
+    for (final Instance instance : ImmutableList.copyOf(this.instances))
+      this.stopService(instance);
+  }
 
-	private void stopService(final Instance instance) {
-		this.instances.remove(instance);
-		instance.end();
-	}
+  private void stopService(final Instance instance) {
+    this.instances.remove(instance);
+    instance.end();
+  }
 }
