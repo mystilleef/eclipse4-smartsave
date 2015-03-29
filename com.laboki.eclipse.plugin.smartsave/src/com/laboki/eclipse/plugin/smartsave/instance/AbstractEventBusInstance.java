00b26883d36d5640569e732b8a5e1aaec99ba75e
Lateef Alabi-Oki <mystilleef@gmail.com>
2015-03-29 04:57:38 -0400

    Possible fix for FileSyncer register errors: Issue 11 on GitHub.

--- com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/instance/AbstractEventBusInstance.java
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/instance/AbstractEventBusInstance.java
@@ -6,6 +6,7 @@ import com.laboki.eclipse.plugin.smartsave.main.EventBus;
 public abstract class AbstractEventBusInstance implements Instance {
 
   protected final EventBus eventBus;
+  boolean isRegistered = false;
 
   protected AbstractEventBusInstance(final EventBus eventBus) {
     this.eventBus = eventBus;
@@ -13,13 +14,17 @@ public abstract class AbstractEventBusInstance implements Instance {
 
   @Override
   public Instance begin() {
+    if (this.isRegistered) return this;
     this.eventBus.register(this);
+    this.isRegistered = true;
     return this;
   }
 
   @Override
   public Instance end() {
+    if (!this.isRegistered) return this;
     this.eventBus.unregister(this);
+    this.isRegistered = false;
     return this;
   }
 }
