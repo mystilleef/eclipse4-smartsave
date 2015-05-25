396b5b73420f97805c592b29bda1e87399baf769
Lateef Alabi-Oki <mystilleef@gmail.com>
2015-05-24 21:20:38 -0400

    Move listener implementation from inner class to parent class

--- com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/main/PartMonitor.java
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/main/PartMonitor.java
@@ -8,17 +8,61 @@ import org.eclipse.ui.IWorkbenchPart;
 import com.google.common.base.Optional;
 import com.laboki.eclipse.plugin.smartsave.instance.Instance;
 
-public final class PartMonitor implements Instance {
+public final class PartMonitor implements Instance, IPartListener {
 
+	private final Instance partServices = new PartServices();
 	private static final Optional<IPartService> SERVICE =
 		EditorContext.getPartService();
-	private static final PartListener LISTENER = new PartListener();
+
+	public PartMonitor() {
+		this.initPartServices();
+	}
+
+	private void
+	initPartServices() {
+		final Optional<IEditorPart> editor = EditorContext.getEditor();
+		if (editor.isPresent()) this.startPartServices(editor.get());
+	}
+
+	@Override
+	public void
+	partActivated(final IWorkbenchPart part) {
+		this.startPartServices(part);
+	}
+
+	private void
+	startPartServices(final IWorkbenchPart part) {
+		if (EditorContext.isEditorPart(part)) this.partServices.start();
+	}
+
+	@Override
+	public void
+	partClosed(final IWorkbenchPart part) {}
+
+	@Override
+	public void
+	partBroughtToTop(final IWorkbenchPart part) {}
+
+	@Override
+	public void
+	partDeactivated(final IWorkbenchPart part) {
+		this.stopPartServices(part);
+	}
+
+	private void
+	stopPartServices(final IWorkbenchPart part) {
+		if (EditorContext.isEditorPart(part)) this.partServices.stop();
+	}
+
+	@Override
+	public void
+	partOpened(final IWorkbenchPart part) {}
 
 	@Override
 	public Instance
 	start() {
 		if (!PartMonitor.SERVICE.isPresent()) return this;
-		PartMonitor.SERVICE.get().addPartListener(PartMonitor.LISTENER);
+		PartMonitor.SERVICE.get().addPartListener(this);
 		return this;
 	}
 
@@ -26,56 +70,7 @@ public final class PartMonitor implements Instance {
 	public Instance
 	stop() {
 		if (!PartMonitor.SERVICE.isPresent()) return this;
-		PartMonitor.SERVICE.get().removePartListener(PartMonitor.LISTENER);
+		PartMonitor.SERVICE.get().removePartListener(this);
 		return this;
-	}
-
-	private static final class PartListener implements IPartListener {
-
-		private final Instance partServices = new PartServices();
-
-		public PartListener() {
-			this.initPartServices();
-		}
-
-		private void
-		initPartServices() {
-			final Optional<IEditorPart> editor = EditorContext.getEditor();
-			if (editor.isPresent()) this.startPartServices(editor.get());
-		}
-
-		@Override
-		public void
-		partActivated(final IWorkbenchPart part) {
-			this.startPartServices(part);
-		}
-
-		private void
-		startPartServices(final IWorkbenchPart part) {
-			if (EditorContext.isEditorPart(part)) this.partServices.start();
-		}
-
-		@Override
-		public void
-		partClosed(final IWorkbenchPart part) {}
-
-		@Override
-		public void
-		partBroughtToTop(final IWorkbenchPart part) {}
-
-		@Override
-		public void
-		partDeactivated(final IWorkbenchPart part) {
-			this.stopPartServices(part);
-		}
-
-		private void
-		stopPartServices(final IWorkbenchPart part) {
-			if (EditorContext.isEditorPart(part)) this.partServices.stop();
-		}
-
-		@Override
-		public void
-		partOpened(final IWorkbenchPart part) {}
 	}
 }
