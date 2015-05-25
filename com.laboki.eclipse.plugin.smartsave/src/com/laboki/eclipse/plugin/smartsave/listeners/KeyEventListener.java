acb239dab2e861577445bdc96c29b054efdf25e0
Lateef Alabi-Oki <mystilleef@gmail.com>
2015-05-25 00:57:42 -0400

    Minor refactor

--- com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/listeners/KeyEventListener.java
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/listeners/KeyEventListener.java
@@ -3,7 +3,6 @@ package com.laboki.eclipse.plugin.smartsave.listeners;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.IEditorPart;
 
 import com.google.common.base.Optional;
 import com.laboki.eclipse.plugin.smartsave.contexts.EditorContext;
@@ -16,9 +15,7 @@ public final class KeyEventListener extends BaseListener
 
 	private static Optional<Control>
 	getControl() {
-		final Optional<IEditorPart> editor = EditorContext.getEditor();
-		if (!editor.isPresent()) return Optional.absent();
-		return EditorContext.getControl(editor);
+		return EditorContext.getControl(EditorContext.getEditor());
 	}
 
 	@Override
--- com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/listeners/VerifyEventListener.java
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/listeners/VerifyEventListener.java
@@ -3,7 +3,6 @@ package com.laboki.eclipse.plugin.smartsave.listeners;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.VerifyEvent;
 import org.eclipse.swt.events.VerifyListener;
-import org.eclipse.ui.IEditorPart;
 
 import com.google.common.base.Optional;
 import com.laboki.eclipse.plugin.smartsave.contexts.EditorContext;
@@ -16,9 +15,7 @@ public final class VerifyEventListener extends BaseListener
 
 	private static Optional<StyledText>
 	getBuffer() {
-		final Optional<IEditorPart> editor = EditorContext.getEditor();
-		if (!editor.isPresent()) return Optional.absent();
-		return EditorContext.getBuffer(editor);
+		return EditorContext.getBuffer(EditorContext.getEditor());
 	}
 
 	@Override
