4c7701db4a124af95a961a2d5d39f638e1897d21
Lateef Alabi-Oki <mystilleef@gmail.com>
2015-05-25 01:08:58 -0400

    Cleanup Scheduler and remove unneeded events and code

--- com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/main/Scheduler.java
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/main/Scheduler.java
@@ -5,8 +5,6 @@ import com.google.common.eventbus.Subscribe;
 import com.laboki.eclipse.plugin.smartsave.contexts.EditorContext;
 import com.laboki.eclipse.plugin.smartsave.events.AssistSessionEndedEvent;
 import com.laboki.eclipse.plugin.smartsave.events.AssistSessionStartedEvent;
-import com.laboki.eclipse.plugin.smartsave.events.DisableSaveListenersEvent;
-import com.laboki.eclipse.plugin.smartsave.events.EnableSaveListenersEvent;
 import com.laboki.eclipse.plugin.smartsave.events.ScheduleSaveEvent;
 import com.laboki.eclipse.plugin.smartsave.events.StartSaveScheduleEvent;
 import com.laboki.eclipse.plugin.smartsave.instance.EventBusInstance;
@@ -19,19 +17,21 @@ public final class Scheduler extends EventBusInstance {
 	public static final int DELAY = 10;
 	public static final TaskMutexRule RULE = new TaskMutexRule();
 	public static final String FAMILY = "SmartSaveSchedulerTaskFamily";
-	boolean completionAssistantIsActive;
+	boolean canSchedule = true;
 
 	@Subscribe
+	@AllowConcurrentEvents
 	public void
 	eventHandler(final AssistSessionStartedEvent event) {
-		this.completionAssistantIsActive = true;
+		this.canSchedule = false;
 		Scheduler.cancelTasks();
 	}
 
 	@Subscribe
+	@AllowConcurrentEvents
 	public void
 	eventHandler(final AssistSessionEndedEvent event) {
-		this.completionAssistantIsActive = false;
+		this.canSchedule = true;
 	}
 
 	@Subscribe
@@ -43,8 +43,8 @@ public final class Scheduler extends EventBusInstance {
 			@Override
 			public boolean
 			shouldSchedule() {
-				if (Scheduler.this.completionAssistantIsActive) return false;
-				return BaseTask.noTaskFamilyExists(BaseTask.FAMILY);
+				if (!Scheduler.this.canSchedule) return false;
+				return BaseTask.noTaskFamilyExists(Scheduler.FAMILY);
 			}
 
 			@Override
@@ -59,29 +59,7 @@ public final class Scheduler extends EventBusInstance {
 			.start();
 	}
 
-	@Subscribe
-	public static void
-	eventHandler(final EnableSaveListenersEvent event) {
-		new Task() {
-
-			@Override
-			public void
-			execute() {
-				EventBus.post(new ScheduleSaveEvent());
-			}
-		}.setDelay(Scheduler.DELAY)
-			.setFamily(Scheduler.FAMILY)
-			.setRule(Scheduler.RULE)
-			.start();
-	}
-
-	@Subscribe
-	public static void
-	eventHandler(final DisableSaveListenersEvent event) {
-		Scheduler.cancelTasks();
-	}
-
-	static void
+	private static void
 	cancelTasks() {
 		EditorContext.cancelSaverTasks();
 	}
