0833eea227b6d26169bca1949bcf1548b13969e8
Lateef Alabi-Oki <mystilleef@gmail.com>
2015-05-26 04:16:09 -0400

    Add preferences checker

---
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/contexts/PreferencesChecker.java
@@ -0,0 +1,40 @@
+package com.laboki.eclipse.plugin.smartsave.contexts;
+
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.Subscribe;
+import com.laboki.eclipse.plugin.smartsave.events.CheckBlacklistEvent;
+import com.laboki.eclipse.plugin.smartsave.events.CheckPreferencesEvent;
+import com.laboki.eclipse.plugin.smartsave.instance.EventBusInstance;
+import com.laboki.eclipse.plugin.smartsave.main.EventBus;
+import com.laboki.eclipse.plugin.smartsave.main.Scheduler;
+import com.laboki.eclipse.plugin.smartsave.preferences.Store;
+import com.laboki.eclipse.plugin.smartsave.task.Task;
+
+public final class PreferencesChecker extends EventBusInstance {
+
+	public PreferencesChecker() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Subscribe
+	@AllowConcurrentEvents
+	public static void
+	eventHandler(final CheckPreferencesEvent event) {
+		new Task() {
+
+			@Override
+			public void
+			execute() {
+				if (Store.getCanSaveAutomatically()) this.broadcastEvent();
+			}
+
+			private void
+			broadcastEvent() {
+				EventBus.post(new CheckBlacklistEvent());
+			}
+		}.setDelay(Scheduler.DELAY)
+			.setFamily(Scheduler.FAMILY)
+			.setRule(Scheduler.RULE)
+			.start();
+	};
+}
