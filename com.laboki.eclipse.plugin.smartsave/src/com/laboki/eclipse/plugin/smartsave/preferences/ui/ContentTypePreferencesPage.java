1bffb3775138940a6a2b34f479beaaf5bd96f579
Lateef Alabi-Oki <mystilleef@gmail.com>
2015-05-26 17:53:32 -0400

    Add needed constructor

--- com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/preferences/ui/ContentTypePreferencesPage.java
+++ com.laboki.eclipse.plugin.smartsave/src/com/laboki/eclipse/plugin/smartsave/preferences/ui/ContentTypePreferencesPage.java
@@ -17,6 +17,8 @@ public final class ContentTypePreferencesPage extends PreferencePage
 	implements
 		IWorkbenchPreferencePage {
 
+	public ContentTypePreferencesPage() {}
+
 	public ContentTypePreferencesPage(final String title) {
 		super(title);
 	}
